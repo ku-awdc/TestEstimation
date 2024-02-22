851806fb3eed0b5e93af88f89137500daa258852
mdenwood <md@sund.ku.dk>
2024-02-22 16:29:57 +0100

    Update sample_sizes.R

--- notebooks/sample_sizes.R
+++ notebooks/sample_sizes.R
@@ -12,3 +12,20 @@ tibble(
   RefSp = 1,
 
 )
+
+## With known reference standard:
+
+tibble(
+  Hyperprior = c("Alpha","Beta"),
+  Se = c(20,2),
+  Sp = c(40,1)
+) ->
+  test_ref
+
+
+tibble(
+  Se = 0.8,
+  Sp = 0.98
+) ->
+  test_comp
+
