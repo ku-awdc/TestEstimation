88aae32eebaf2ca57ef640997f2b127129448cfc
Matt Denwood <md@sund.ku.dk>
2024-02-22 10:18:25 +0100

    Create sample_sizes.R

---
+++ notebooks/sample_sizes.R
@@ -0,0 +1,14 @@
+## Ideas for sample size stuff
+
+library("tidyverse")
+library("TeachingDemos")
+library("coda")
+library("runjags")
+
+## With gold standard:
+
+tibble(
+  RefSe = 1,
+  RefSp = 1,
+
+)
